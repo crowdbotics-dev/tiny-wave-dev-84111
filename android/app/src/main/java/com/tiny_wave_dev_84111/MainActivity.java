d6a065c6ad3b452cc61ae5a1f157ab60b4c02091
Crowdbotics <engineering@crowdbotics.com>
2023-05-17 23:10:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tiny_wave_dev_84111/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tiny_wave_dev_84111;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tiny_wave_dev_84111";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-wave-dev-84111",
+  url: "https://tiny-wave-dev-84111.botics.co",
+  api: "https://tiny-wave-dev-84111.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
